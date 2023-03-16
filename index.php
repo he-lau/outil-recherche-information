6167742cf4b852efed20e9e105d3bec940336848
he-lau <he.laurent.1130@gmail.com>
2023-03-16 21:36:07 +0100

    MAJ regex + tokenisation + init BDD

--- index.php
+++ index.php
@@ -7,26 +7,25 @@ function console_log($msg, $content='') {
 function read_file($path, $separator=" ") {
     console_log("Lecture du fichier",$path);
     $contenuFichier = file_get_contents($path);
-    console_log("Longueur du fichier",strlen($contenuFichier));    
+    console_log("Longueur du fichier",strlen($contenuFichier));
     //echo strlen($contenuFichier);
     return explode($separator,$contenuFichier);
 }
 
-function clean_array($array) {
+function clean_array($array,$path) {
+    console_log("Nettoyage des mots",$path);
     // minuscule
     $array = array_map('strtolower', $array);
     // espace
     $array = array_map('trim', $array);
 
-    // virgule, point & point-virgule
-    $array = array_map(function ($value) {
-        return preg_replace('/[,;.]+\s*$/', '', $value);
-    }, $array);
+    // garder que les mots
+    //$array = preg_grep('/^[a-zA-Z]{2,}$/', $array);
 
-    // parentheses debut/fin
-    $array = array_map(function ($value) {
-        return preg_replace('/^[\(\)]+|[\(\)]+$/', '', $value);
-    }, $array);
+    // garder les mots et les chiffres
+    $array = preg_grep('/^[a-z0-9]{2,}$/', $array);
+
+
 
     return $array;
 }
@@ -38,37 +37,42 @@ function remove_stopwords($file, $stopwords, $path) {
     $count = 0;
 
     // parcours de l'ensemble des mots
-    foreach ($file as $key => $word) {        
+    foreach ($file as $key => $word) {
         // si le mot est présent dans les stopwords, le supprimer de la liste
         if (in_array($word, $stopwords)) {
             unset($file[$key]);
             $count++;
         }
     }
-    console_log("$count stopwords supprimés",$path);    
+    console_log("[FIN] Suppression des stopwords",$path);
+    console_log("$count stopwords supprimés",$path);
     return $file;
 }
 
-function indexation($content,$path) {
-    console_log("Indexation des mots",$path);
+function tokenisation($content,$path) {
+    console_log("tokenisation des mots",$path);
 
     $occurrences = array();
 
     foreach($content as $word) {
+        // si l'occurence est deja dans le tableau on l'incremente
         if(isset($occurrences[$word])) {
             $occurrences[$word]++;
         } else {
+            // init à 1
             $occurrences[$word] = 1;
         }
     }
-    
+
+    console_log("[FIN] tokenisation des mots",$path);
+
     return $occurrences;
 }
 
 function afficher_liste_html($occurrences,$path) {
     // Tri du tableau par ordre décroissant de la valeur (nombre d'apparitions)
     arsort($occurrences);
-    
+
     // Affichage de la liste à puces HTML
     echo "<h2>$path</h2>";
     echo "<ul>";
@@ -86,26 +90,32 @@ function afficher_liste_html($occurrences,$path) {
 
 <?php
     // lecture des fichiers
-    // TODO : parcours recursive 
-    $cheminFichier = './docs/jdg.txt';
+    // TODO : parcours recursive
+    $cheminFichier = './docs/web.txt';
     $contenuFichier = read_file($cheminFichier);
 
     $stop_path = './stopwordsv2.txt';
     $stop_content = read_file($stop_path,"\n");
 
     // nettoyage
-    $contenuFichier = clean_array($contenuFichier);
-    $stop_content = clean_array($stop_content);
-    
+    $contenuFichier = clean_array($contenuFichier,$cheminFichier);
+    $stop_content = clean_array($stop_content,$stop_path);
+
     // suppression des stopwords
     $contenuFichier = remove_stopwords($contenuFichier,$stop_content,$cheminFichier);
 
     //var_dump(implode("<br>", $contenuFichier));
 
-    $indexation = indexation($contenuFichier,$cheminFichier);
-    afficher_liste_html($indexation,$cheminFichier);
-    
+    $tokenisation = tokenisation($contenuFichier,$cheminFichier);
+    afficher_liste_html($tokenisation,$cheminFichier);
 
 
 
+
+?>
+
+<?php
+    require_once "php/functions_db.php";
+
+    connect_db();
 ?>
